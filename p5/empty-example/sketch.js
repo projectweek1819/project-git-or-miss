5beedc76f3fa20d00497f85f58cda872990c29d5
Stef Tweepenninckx <stef.tweepenninckx@student.kuleuven.be>
2018-12-20 15:28:06 +0100

    swap werkt

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -3,68 +3,116 @@ var s = function( p ) {
 
 
   p.setup = function() {
-    p.createCanvas(500,500);
+    p.createCanvas(600,500);
     p.createGrid();
-    p.validateGrid();
   };
 
+  p.isInside = function(position){
+    if(position.x < p.grid[0].length && position.y < p.grid.length && position.x > -1 && position.y > -1) {
+        return true;
+    } else {return false;};
+}
   p.randomColor = function(){
     colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff33ff'];
     var rand = colors[Math.floor(Math.random() * colors.length)];
     return rand;
   };
   
+  p.sleep = function(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  p.gridPosition = function(valueX, valueY){
+    console.log(valueX, valueY)
+    if  (valueX <0.3 && valueX > -0.3){
+      gridX = 0;
+    } 
+    else if (0.7 < valueX && valueX <1.3) {
+      gridX = 1;
+    }
+    else if (1.7 <= valueX && valueX <2.3){
+      gridX = 2;
+    } 
+    else if (2.7 <= valueX && valueX <3.3){
+      gridX = 3;
+    } 
+    else if (3.7 <= valueX && valueX <4.3){
+      gridX = 4;
+    } 
+    else {
+      gridX = null;
+    }
+    if (-0.3 <= valueY && valueY <0.3) gridY = 0;
+    else if (0.7 <= valueY && valueY  <1.3) gridY = 1;
+    else if (1.7 <= valueY && valueY <2.3) gridY = 2;
+    else if (2.7 <= valueY && valueY <3.3) gridY = 3;
+    else if (3.7 <= valueY && valueY <4.3) gridY = 4;
+    else {
+      gridY =null;
+    }
+    return {x: gridX, y: gridY}    
+  }
   p.draw = function() {
-    p.background(10000);
-    for (var x = 0; x < p.grid[0].length; x++) {
-      for (var y = 0; y < p.grid.length; y++) {
-        p.ellipse(x*100+60, y*100+60, 60,60);
+    p.background(0);
+    for (var y = 0; y < p.grid.length; y++) {
+      for (var x = 0; x < p.grid[0].length; x++) {
         p.fill(p.grid[y][x].color);
-       
+        p.ellipse(x*100+60, y*100+60, 60,60);
+        p.sleep(2000);
       }
     }
   };
 
-  
+  p.mousePressed = function() {
+    mousePos=p.gridPosition((p.mouseX-60)/100,(p.mouseY-60)/100)
+    if (mousePos.x == null || mousePos.y == null){
+      return;
+    }
+    else{
+      p.grid[mousePos.y][mousePos.x].clicked = true;
+    }
+    p.checkSwap();
+  }
   p.createGrid = function(){
     let a = [];    
-    for (var x = 0; x < 5; x++) {
+    for (var y = 0; y < 5; y++) {
       let row = [];
-      for (var y = 0; y < 5; y++) {
-        let temp = {position : {x, y}, color : p.randomColor()};
+      for (var x = 0; x < 5; x++) {
+        let temp = {position : {x, y}, color : p.randomColor(), clicked:false};
         row.push(temp);
       }
+      console.log(row);
       a.push(row);
     }
     p.grid = JSON.parse(JSON.stringify(a));
   };
 
-  p.validateGrid = function(){
- /*   let a = JSON.parse(JSON.stringify(p.grid));
-    let row0= a[0];
-    let row1= a[1];
-    let row2= a[2];
-    let row3= a[3];
-    let row4= a[4];
-    for (var i = 0; i <row0.length; i++){
-      var count = 0;
-      if (row0[i].color === row0[i+1].color){
-        count++;
-        for (var j = 1; j + i < row0.length; j++){
-          if (row0[i].color === row0[i+1+j].color){
-            count++;
-          }
+  p.checkSwap = function(){
+    var count = 0;
+    let counted = [];
+    for (var y = 0; y < 5; y++) {
+      for (var x = 0; x < 5; x++) {
+        if(p.grid[y][x].clicked){
+          count++;
+          counted.push(p.grid[y][x])
         }
-        if (count >= 3){
-
-        }
-
       }
-    }*/
-
-  };
+    }
+    if (count ===2){
+      counted[0].clicked = false;
+      counted[1].clicked = false;
+      p.swap(counted[0], counted[1]);
+    }
+  }
+  p.swap = function(p1, p2){
+    temp = p1.color;
+    p1.color = p2.color;
+    p2.color = temp;
+    p.draw();
+  }
 
 };
 
 var myp5 = new p5(s);
+
 
